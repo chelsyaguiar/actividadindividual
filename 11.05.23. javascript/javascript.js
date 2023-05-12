bf9063a855917cb90aa87fd91d0945d6d1f96c0f
chelsyaguiar <aguiarguerreroc@gmail.com>
2023-05-11 22:07:43 -0500

    ejercicio calcular descuento

---
+++ 11.05.23. javascript/javascript.js
@@ -0,0 +1,13 @@
+function calcularprecio(preciobase, descuento){
+if(preciobase <= 0 || descuento <0 || descuento > 100)
+    {return "Los valores ingresados son inválidos"};
+
+    if (descuento == 0)
+    {return " precio base sin descuento" + preciobase}; 
+
+    if (descuento > 0 && descuento <= 100){
+        var preciofinal = preciobase - preciobase * descuento / 100;
+        return " El precio final con descuento es $" + preciofinal
+    }
+}
+console.log (calcularprecio (-12, 15))
